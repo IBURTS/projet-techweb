3dc2468d1e8876a9edbbecd3d299733fb006e068
AlecImhofUni <115621204+AlecImhofUni@users.noreply.github.com>
2023-05-27 15:44:05 +0200

    class User for SQLAlchemy

--- chat.py
+++ chat.py
@@ -6,43 +6,81 @@ app.config['SECRET_KEY'] = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 db = SQLAlchemy(app)
 
-class User:
+"""
+class ExampleUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    profession = db.Column(db.String(80), nullable=False)
+
     def __init__(self, name, email, gender, age, profession):
-        self.name = name 
+        self.name = name
         self.email = email
         self.gender = gender
-        self.age = age 
-        self.profession = profession 
-obj = User("John","john.doe@unien.ch", None, 0 , None) 
+        self.age = age
+        self.profession = profession
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text)
+
+    def __init__(self, author, content):
+        self.author = author
+        self.content = content
+
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+"""
+
+
+class User:
+    def __init__(self, name, email, gender, age, profession):
+        self.name = name
+        self.email = email
+        self.gender = gender
+        self.age = age
+        self.profession = profession
+
+
+obj = User("John", "john.doe@unien.ch", None, 0, None)
+
 
 @app.route('/')
 def logIn():
-    return  render_template('logIn.html', name=obj.name)
+    return render_template('logIn.html', name=obj.name)
+
 
 @app.route('/create_user', methods=['POST'])
 def create_user():
     name = request.form['name']
     email = request.form['email']
     gender = request.form['gender']
-    #if gender != "male" or gender != "femmale" or gender != "other":
+    # if gender != "male" or gender != "female" or gender != "other":
     #    return render_template
-    #faire un message d'erreur maybe 
+    # faire un message d'erreur maybe
     # -> trouver comme faire des messages d'erreur comme ceux qui existent déjà ex: impossible de mettre un email sans "@"
     age = request.form['age']
     profession = request.form['profession']
-    user = User(name,email,gender,age,profession)
+    user = User(name, email, gender, age, profession)
     session['name'] = request.form['name']
     session['email'] = request.form['email']
     session['gender'] = request.form['gender']
     session['age'] = request.form['age']
     session['profession'] = request.form['profession']
     return render_template(
-        'home.html', 
-        name = name, 
+        'home.html',
+        name=name,
         email=email,
         gender=gender,
         age=age,
         profession=profession)
+
 
 @app.route('/goToHome', methods=["POST"])
 def goToHome():
@@ -53,11 +91,12 @@ def goToHome():
     profession = session.get('profession')
     return render_template(
         'home.html',
-        name = name ,
-        email = email,  
-        gender = gender,
-        age = age,
-        profession = profession)
+        name=name,
+        email=email,
+        gender=gender,
+        age=age,
+        profession=profession)
+
 
 @app.route('/goToAccount')
 def goToAccount():
@@ -67,27 +106,31 @@ def goToAccount():
     age = session.get('age')
     profession = session.get('profession')
     return render_template(
-        'account.html', 
-        name = name ,
-        email = email,  
-        gender = gender,
-        age = age,
-        profession = profession)
+        'account.html',
+        name=name,
+        email=email,
+        gender=gender,
+        age=age,
+        profession=profession)
+
 
 @app.route('/logout')
 def logout():
     session.pop('name', None)
     session.pop('email', None)
-    session.pop('gender',None)
-    session.pop('age',None)
-    session.pop('profession',None)
+    session.pop('gender', None)
+    session.pop('age', None)
+    session.pop('profession', None)
     return redirect(url_for('logIn'))
 
-# A PARTIR DE LA C'EST BAGDAD
+
+# A PARTIR DE LA C'EST BAGHDAD
+
 
 @app.route('/goToModifyAccount')
 def goToModifyAccount():
     return redirect(url_for('modifyAccount'))
+
 
 @app.route('/modifyAccount')
 def modifyAccount():
